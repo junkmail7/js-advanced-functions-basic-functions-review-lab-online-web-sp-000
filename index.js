892c67c81b03fa80e1ba261f76cbe6f4fcb8ff4a
William Pannell <lpann5@gmail.com>
2021-02-02 18:19:06 -0600

    commit tree 3ae090bf60130fb8a023a954146ca3fcf8c84d80

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office') {
+   return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(flair = '*'){
+    return function(def = 'special') {
+        return `You are ${flair}${def}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){
+        return a+b
+    },
+    subtract: function(a,b){
+        return a-b
+    },
+    multiply: function(a,b){
+        return a*b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(startInteger, fnArray){
+    let a = startInteger
+
+    for (let i = 0; i < fnArray.length; i++ ){
+      a = fnArray[i](a)
+    }
+
+    return a
+}
